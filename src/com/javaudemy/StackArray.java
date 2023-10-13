62611ff46408e01818c19a4551460fa6c7a3fed5
majestymewtwo <majestymewtwo@gmail.com>
2023-10-13 20:37:11 +0530

    added arraylist-implemented stack

---
+++ src/com/javaudemy/StackArray.java
@@ -0,0 +1,43 @@
+package com.javaudemy;
+
+import java.util.ArrayList;
+
+public class StackArray<T> {
+    private ArrayList<T> stackList = new ArrayList<>();
+
+    public ArrayList<T> getStackList() {
+        return stackList;
+    }
+
+    public void printStack() {
+        for (int i = stackList.size()-1; i >= 0; i--) {
+            System.out.println(stackList.get(i));
+        }
+    }
+
+    public boolean isEmpty() {
+        return stackList.size() == 0;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stackList.get(stackList.size() - 1);
+        }
+    }
+
+    public int size() {
+        return stackList.size();
+    }
+    public void push(T value){
+        stackList.add(value);
+    }
+    public T pop(){
+        int size = stackList.size();
+        if(size == 0) return null;
+        T element = stackList.get(size - 1);
+        stackList.remove(size - 1);
+        return element;
+    }
+}
